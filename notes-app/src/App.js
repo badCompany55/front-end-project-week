5e6567e7fbd3926d2aeb52b6448512d1ff5e7564
dickwinters <coding1965@gmail.com>
2019-02-11 22:56:11 -0600

    POST add post request, add form, update state accordingly

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import {Route} from 'react-router-dom';
 import NoteList from './comps/noteList.js';
+import Form from './comps/noteForm.js';
 
 class App extends Component {
   constructor(props) {
@@ -23,12 +24,22 @@ class App extends Component {
       });
   }
 
+  updateState = note => {
+    let newNotes = {...this.state};
+    newNotes.notes.push(note);
+    this.setState(newNotes);
+  };
+
   render() {
     return (
       <div className="App">
         <Route
           path="/"
           render={props => <NoteList {...props} notes={this.state.notes} />}
+        />
+        <Route
+          path="/new"
+          render={props => <Form {...props} updateState={this.updateState} />}
         />
       </div>
     );
---
+++ notes-app/src/comps/noteForm.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import axios from 'axios';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tags: [],
+      title: '',
+      textBody: '',
+    };
+  }
+
+  capInput = e => {
+    this.setState({[e.target.id]: e.target.value});
+  };
+
+  subHandle = e => {
+    e.preventDefault();
+    const newNote = {...this.state};
+    console.log(newNote);
+    axios
+      .post('https://fe-notes.herokuapp.com/note/create', newNote)
+      .then(res => {
+        console.log(res);
+        newNote.id = res.data.success;
+        this.props.updateState(newNote);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    this.setState({tags: [], title: '', content: ''});
+  };
+
+  render() {
+    return (
+      <div className="formCont">
+        <form className="noteForm" onSubmit={this.subHandle}>
+          <div className="titleCont">
+            <label className="title" htmlFor="title">
+              Title:{' '}
+            </label>
+            <input
+              id="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.capInput}
+            />
+          </div>
+          <div className="contentCont">
+            <label className="content" htmlFor="content">
+              Content:{' '}
+            </label>
+            <textarea
+              id="textBody"
+              name="content"
+              cols="30"
+              rows="10"
+              value={this.state.content}
+              onChange={this.capInput}
+            />
+          </div>
+          <button>Add</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
