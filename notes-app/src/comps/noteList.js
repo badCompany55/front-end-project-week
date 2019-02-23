93e72df85c432e61603a225f8b983c3f335ff652
badCompany55 <zachirvin45@gmail.com>
2019-02-22 18:42:23 -0600

    GSAP add inital load animation

--- notes-app/src/comps/noteList.js
+++ notes-app/src/comps/noteList.js
@@ -1,21 +1,33 @@
 import React from "react";
 import Note from "./note.js";
 import DeleteConf from "./deleteConf.js";
+import { TimelineLite, TweenLite, TweenMax, CSSPlugin } from "gsap/all";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-const NoteList = props => {
-  const idPass = target => {
-    props.stateId(target);
+class NoteList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.notes = [];
+    this.tl = new TimelineLite({});
+    // this.tm = new TweenLite({});
+  }
+
+  componentDidMount() {
+    this.tl.fromTo(this.notes, 0.75, { opacity: 0 }, { opacity: 1 });
+  }
+
+  idPass = target => {
+    this.props.stateId(target);
   };
 
-  const sortAscend = () => {
-    const titles = props.notes.map(note => {
+  sortAscend = () => {
+    const titles = this.props.notes.map(note => {
       return note.title;
     });
     let sortedA = titles.sort();
     let finalSort = [];
     sortedA.forEach(title => {
-      props.notes.forEach(note => {
+      this.props.notes.forEach(note => {
         if (note.title === title) {
           finalSort.push(note._id);
         }
@@ -30,24 +42,24 @@ const NoteList = props => {
 
     let theFinal = [];
     finalNoDups.forEach(id => {
-      props.notes.forEach(note => {
+      this.props.notes.forEach(note => {
         if (note._id === id) {
           theFinal.push(note);
         }
       });
     });
 
-    props.sortNotes(theFinal);
+    this.props.sortNotes(theFinal);
   };
 
-  const sortDescend = () => {
-    const titles = props.notes.map(note => {
+  sortDescend = () => {
+    const titles = this.props.notes.map(note => {
       return note.title;
     });
     let sortedA = titles.sort();
     let finalSort = [];
     sortedA.forEach(title => {
-      props.notes.forEach(note => {
+      this.props.notes.forEach(note => {
         if (note.title === title) {
           finalSort.push(note._id);
         }
@@ -62,7 +74,7 @@ const NoteList = props => {
 
     let theFinal = [];
     finalNoDups.forEach(id => {
-      props.notes.forEach(note => {
+      this.props.notes.forEach(note => {
         if (note._id === id) {
           theFinal.push(note);
         }
@@ -71,15 +83,15 @@ const NoteList = props => {
 
     let theOtherFinal = theFinal.reverse();
 
-    props.sortNotes(theOtherFinal);
+    this.props.sortNotes(theOtherFinal);
   };
 
-  const theDefault = () => {
-    props.refresh();
+  theDefault = () => {
+    this.props.refresh();
   };
 
-  const onDragEnd = result => {
-    let notes = props.notes;
+  onDragEnd = result => {
+    let notes = this.props.notes;
     let movedNote = notes.slice(result.source.index, result.source.index + 1);
     let workingNotes = notes.filter(note => {
       if (note._id !== movedNote[0]._id) {
@@ -94,62 +106,65 @@ const NoteList = props => {
     // console.log("working:", workingNotes);
     // console.log(notes.length, workingNotes.length);
     // console.log("new:", newNotes);
-    props.sortState(newNotes);
+    this.props.sortState(newNotes);
   };
 
-  return (
-    <div className="listCont">
-      <div className="sorting">
-        <i className="fas fa-arrow-circle-up" onClick={sortAscend} />
-        <i className="fas fa-home" onClick={theDefault} />
-        <i className="fas fa-arrow-circle-down" onClick={sortDescend} />
+  render() {
+    return (
+      <div className="listCont">
+        <div className="sorting">
+          <i className="fas fa-arrow-circle-up" onClick={this.sortAscend} />
+          <i className="fas fa-home" onClick={this.theDefault} />
+          <i className="fas fa-arrow-circle-down" onClick={this.sortDescend} />
+        </div>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable">
+            {provided => (
+              <div className="droppableCont" ref={provided.innerRef}>
+                {this.props.notes.map((note, index) => {
+                  return (
+                    <Draggable
+                      key={note._id}
+                      draggableId={note._id}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <div ref={div => (this.notes[index] = div)}>
+                            <Note
+                              key={note._id}
+                              id={note._id}
+                              noteName={note.title}
+                              noteContent={note.textBody}
+                              etarget={this.idPass}
+                              currentResult={this.props.currentResult}
+                              index={index}
+                              dragging={snapshot.isDragging}
+                            />
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                  );
+                })}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+        <div className="deleteBox">
+          <DeleteConf
+            {...this.props}
+            noteDelete={this.props.noteDelete}
+            modal={this.props.modal}
+            resetModal={this.props.resetModal}
+          />
+        </div>
       </div>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable">
-          {provided => (
-            <div className="droppableCont" ref={provided.innerRef}>
-              {props.notes.map((note, index) => {
-                return (
-                  <Draggable
-                    key={note._id}
-                    draggableId={note._id}
-                    index={index}
-                  >
-                    {(provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <Note
-                          key={note._id}
-                          id={note._id}
-                          noteName={note.title}
-                          noteContent={note.textBody}
-                          etarget={idPass}
-                          currentResult={props.currentResult}
-                          index={index}
-                          dragging={snapshot.isDragging}
-                        />
-                      </div>
-                    )}
-                  </Draggable>
-                );
-              })}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-      <div className="deleteBox">
-        <DeleteConf
-          {...props}
-          noteDelete={props.noteDelete}
-          modal={props.modal}
-          resetModal={props.resetModal}
-        />
-      </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 export default NoteList;
