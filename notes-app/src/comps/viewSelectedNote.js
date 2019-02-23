186c58290cc3e9796e761125a506b716d506f3db
badCompany55 <zachirvin45@gmail.com>
2019-02-23 10:44:43 -0600

    BUG fix bug in edit render

--- notes-app/src/comps/viewSelectedNote.js
+++ notes-app/src/comps/viewSelectedNote.js
@@ -3,46 +3,78 @@ import Note from "./note.js";
 import DeleteConf from "./deleteConf.js";
 import { TimelineLite } from "gsap/all";
 
-class SelectedNote extends React.Component {
-  constructor(props) {
-    super(props);
-    this.note = null;
-    this.tl = new TimelineLite({});
-  }
+// class SelectedNote extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       note: null
+//     };
+//     this.tl = new TimelineLite({});
+//   }
+//
+//   componentDidMount() {
+//     this.tl.fromTo(this.state.note, 0.75, { opacity: 0 }, { opacity: 1 });
+//   }
+//
+//   theNote = this.props.notes.find(note => {
+//     return note._id === this.props.match.params.id;
+//   });
+//
+//   idPass = target => {
+//     this.props.stateId(target);
+//   };
+//
+//   render() {
+//     return (
+//       <div className="singNoteCont" ref={div => (this.state.note = div)}>
+//         <Note
+//           key={this.theNote._id}
+//           id={this.theNote._id}
+//           noteName={this.theNote.title}
+//           noteContent={this.theNote.textBody}
+//           etarget={this.idPass}
+//         />
+//         <div className="deleteBox">
+//           <DeleteConf
+//             {...this.props}
+//             noteDelete={this.props.noteDelete}
+//             modal={this.props.modal}
+//             resetModal={this.props.resetModal}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-  componentDidMount() {
-    this.tl.fromTo(this.note, 0.75, { opacity: 0 }, { opacity: 1 });
-  }
-
-  theNote = this.props.notes.find(note => {
-    return note._id === this.props.match.params.id;
+const SelectedNote = props => {
+  const theNote = props.notes.find(note => {
+    return note._id === props.match.params.id;
   });
 
-  idPass = target => {
-    this.props.stateId(target);
+  const idPass = target => {
+    props.stateId(target);
   };
 
-  render() {
-    return (
-      <div className="singNoteCont" ref={div => (this.note = div)}>
-        <Note
-          key={this.theNote._id}
-          id={this.theNote._id}
-          noteName={this.theNote.title}
-          noteContent={this.theNote.textBody}
-          etarget={this.idPass}
+  return (
+    <div className="singNoteCont">
+      <Note
+        key={theNote._id}
+        id={theNote._id}
+        noteName={theNote.title}
+        noteContent={theNote.textBody}
+        etarget={idPass}
+      />
+      <div className="deleteBox">
+        <DeleteConf
+          {...props}
+          noteDelete={props.noteDelete}
+          modal={props.modal}
+          resetModal={props.resetModal}
         />
-        <div className="deleteBox">
-          <DeleteConf
-            {...this.props}
-            noteDelete={this.props.noteDelete}
-            modal={this.props.modal}
-            resetModal={this.props.resetModal}
-          />
-        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SelectedNote;
